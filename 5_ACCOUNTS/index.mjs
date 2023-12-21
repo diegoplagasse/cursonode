ff01128eb861b190d278d95bb415a890cbc7a20c
Diego Lagasse <drodrigues@cbmerj.rj.gov.br>
2023-12-21 19:42:34 -0300

    seção 5 accounts

---
+++ 5_ACCOUNTS/index.mjs
@@ -0,0 +1,70 @@
+//modulos externos
+import inquirer from 'inquirer'
+import chalk from 'chalk'
+
+//modulos internos
+import fs from 'fs'
+
+operation()
+
+function operation() {
+
+    inquirer.prompt([{
+        type: 'list',
+        name: 'action',
+        message: 'O que você deseja fazer?',
+        choices: [ 'Criar conta', 'Consultar saldo', 'Depositar', 'Sacar', 'Sair' ],
+    }]).then( (answer) => {
+        const action = answer['action']
+
+        if (action === 'Criar conta') {
+            createAccount()
+        }
+    })
+    .catch( (err) => console.log(err) )
+
+
+}
+
+//create an account
+function createAccount(){
+
+    console.log(chalk.bgGreen.black('Parabéns por escolher nosso banco!'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir'))
+
+    buildAccount()
+}
+
+function buildAccount() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Digite um nome para a sua conta: '
+        }
+    ]).then( (answer) => {
+
+        const accountName = answer['accountName']
+        console.info( accountName )
+
+        if(!fs.existsSync('accounts')){
+            fs.mkdirSync('accounts')
+        }
+
+        if(fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(
+                chalk.bgRed.black('Esta conta já existe, escolha outro nome!')
+            )
+            buildAccount()
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}',
+        function (err) {
+            console.log(err)
+        },
+        )
+
+        console.log(chalk.green('Parabéns a sua conta foi criada'))
+        operation()
+
+    }).catch( (err) => console.log(err) )
+}
